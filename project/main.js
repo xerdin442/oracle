924c72a0e1a05efd10865955b03320147cc1e29f
xerdin442 <mudianthonio27@gmail.com>
2023-10-04 02:52:33 +0100

    updated codebase

--- project/main.js
+++ project/main.js
@@ -31,3 +31,38 @@ document.querySelector('.process-box').addEventListener('click', function (event
         panel.classList.toggle('hidden');
     }
 });
+
+//Scroll Effect
+window.addEventListener('scroll', reveal);
+function reveal() {
+    let reveals = document.querySelectorAll('.reveal')
+
+    for (let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealtop = reveals[i].getBoundingClientRect().top;
+        let revealpoint = 150
+
+        if (revealtop < windowHeight - revealpoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+
+}
+
+//Back To Top
+let scrollButton = document.getElementById('scrollButton')
+window.onscroll = function () {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollButton.style.display = 'block'
+    } else {
+        scrollButton.style.display = 'none'
+    }
+}
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
