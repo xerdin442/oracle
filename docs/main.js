4a691e4523d6f37c8b4e16a56b82d6fabcec9a7a
xerdin442 <mudianthonio27@gmail.com>
2024-04-10 20:07:21 +0100

    refactored code

--- docs/main.js
+++ docs/main.js
@@ -1,68 +1,93 @@
 //Sidebar
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.getElementById('sidebar');
-const menuOpenIcon = document.getElementById('menu-open');
-const menuCloseIcon = document.getElementById('menu-close');
+const menuToggle = document.querySelector('#menu-toggle');
+const sidebar = document.querySelector('#sidebar');
+const menuOpenIcon = document.querySelector('#menu-open');
+const menuCloseIcon = document.querySelector('#menu-close');
 
 menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('-translate-x-full');
-    menuOpenIcon.classList.toggle("hidden");
-    menuCloseIcon.classList.toggle("hidden");
+  sidebar.classList.toggle('-translate-x-full');
+  menuOpenIcon.classList.toggle("hidden");
+  menuCloseIcon.classList.toggle("hidden");
 });
 
-//Accordion
-document.querySelector('.accordion').addEventListener('click', function (event) {
-    if (event.target.classList.contains('toggle-button')) {
-        const button = event.target;
-        const panel = button.closest('.accordion-item').querySelector('.panel-content');
 
-        // Toggle the visibility of the panel
-        panel.classList.toggle('hidden');
+// Accordion
+function toggleAccordion(panelButton) {
+  let accordion = panelButton.parentNode.parentNode
+  let panel = accordion.querySelector('.panel-content')
+
+  // Toggle the visibility of the panel
+  panel.classList.toggle('hidden');
+  // Close any open panels that is not the clicked panel
+  let panels = document.querySelectorAll('.panel-content')
+  panels.forEach((otherPanel) => {
+    if (otherPanel !== panel) {
+      otherPanel.classList.add('hidden')
     }
-});
-
-//Process
-document.querySelector('.process-box').addEventListener('click', function (event) {
-    if (event.target.classList.contains('process-header')) {
-        const button = event.target;
-        const panel = button.closest('.process-header').querySelector('.process-detail');
-
-        // Toggle the visibility of the detail
-        panel.classList.toggle('hidden');
+  })
+  // Update all toggle buttons based on panel visibility
+  let allButtons = document.querySelectorAll('.toggle-button');
+  allButtons.forEach((button) => {
+    if (button !== panelButton) {
+      button.textContent = '+';
     }
-});
+  });
+  // Change toggle button for clicked panel
+  if (panel.classList.contains('hidden')) {
+    panelButton.textContent = '+'
+  } else {
+    panelButton.textContent = '-'
+  }
+}
 
-//Scroll Effect
+
+// Process
+function toggleDetail(processHeader) {
+  let detail = processHeader.querySelector('.process-detail')
+
+  // Toggle the visibility of the detail
+  detail.classList.toggle('hidden');
+  // Close any open details that is not the clicked detail
+  let details = document.querySelectorAll('.process-detail')
+  details.forEach((otherDetail) => {
+    if (otherDetail !== detail) {
+      otherDetail.classList.add('hidden')
+    }
+  })
+}
+
+
+// Scroll Effect
 window.addEventListener('scroll', reveal);
 function reveal() {
-    let reveals = document.querySelectorAll('.reveal')
+  let reveals = document.querySelectorAll('.reveal')
 
-    for (let i = 0; i < reveals.length; i++) {
-        let windowHeight = window.innerHeight;
-        let revealtop = reveals[i].getBoundingClientRect().top;
-        let revealpoint = 150
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealtop = reveals[i].getBoundingClientRect().top;
+    let revealpoint = 150
 
-        if (revealtop < windowHeight - revealpoint) {
-            reveals[i].classList.add('active');
-        } else {
-            reveals[i].classList.remove('active');
-        }
+    if (revealtop < windowHeight - revealpoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
     }
+  }
 
 }
 
-//Back To Top
-let scrollButton = document.getElementById('scrollButton')
-window.onscroll = function () {scrollFunction()};
+// Back To Top
+let scrollButton = document.querySelector('#scrollButton')
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        scrollButton.style.display = 'block'
-    } else {
-        scrollButton.style.display = 'none'
-    }
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollButton.style.display = 'block'
+  } else {
+    scrollButton.style.display = 'none'
+  }
 }
 function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
